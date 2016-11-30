1a11eef7cf8cd40b6d1fd804eee5770506dfe812
kevin-deboer <kevin.deboer@mounthermon.org>
2016-11-29 17:04:07 -0800

    Copy master to Child
    To use sass correctly I need all the files inside Child so that gulp
    watch functions correctly and actually complies sass to css.

--- functions.php
+++ functions.php
@@ -1,15 +1,51 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+/**
+ * Author: Ole Fredrik Lie
+ * URL: http://olefredrik.com
+ *
+ * FoundationPress functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
+/** Various clean up functions */
+require_once( 'library/cleanup.php' );
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+/** Required for Foundation to work properly */
+require_once( 'library/foundation.php' );
 
-// END ENQUEUE PARENT ACTION
+/** Register all navigation menus */
+require_once( 'library/navigation.php' );
+
+/** Add menu walkers for top-bar and off-canvas */
+require_once( 'library/menu-walkers.php' );
+
+/** Create widget areas in sidebar and footer */
+require_once( 'library/widget-areas.php' );
+
+/** Return entry meta information for posts */
+require_once( 'library/entry-meta.php' );
+
+/** Enqueue scripts */
+require_once( 'library/enqueue-scripts.php' );
+
+/** Add theme support */
+require_once( 'library/theme-support.php' );
+
+/** Add Nav Options to Customer */
+require_once( 'library/custom-nav.php' );
+
+/** Change WP's sticky post class */
+require_once( 'library/sticky-posts.php' );
+
+/** Configure responsive image sizes */
+require_once( 'library/responsive-images.php' );
+
+/** If your site requires protocol relative url's for theme assets, uncomment the line below */
+// require_once( 'library/protocol-relative-theme-assets.php' );
