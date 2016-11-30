11d2e0b06a11611d04cb9ee831ba470055aa55a8
kevin-deboer <kevin.deboer@mounthermon.org>
2016-11-29 18:15:55 -0800

    Setup Child theme... again...

--- functions.php
+++ functions.php
@@ -1,51 +1,14 @@
 <?php
-/**
- * Author: Ole Fredrik Lie
- * URL: http://olefredrik.com
- *
- * FoundationPress functions and definitions
- *
- * Set up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- * @link https://codex.wordpress.org/Theme_Development
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+function my_theme_enqueue_styles() {
 
-/** Various clean up functions */
-require_once( 'library/cleanup.php' );
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-/** Required for Foundation to work properly */
-require_once( 'library/foundation.php' );
-
-/** Register all navigation menus */
-require_once( 'library/navigation.php' );
-
-/** Add menu walkers for top-bar and off-canvas */
-require_once( 'library/menu-walkers.php' );
-
-/** Create widget areas in sidebar and footer */
-require_once( 'library/widget-areas.php' );
-
-/** Return entry meta information for posts */
-require_once( 'library/entry-meta.php' );
-
-/** Enqueue scripts */
-require_once( 'library/enqueue-scripts.php' );
-
-/** Add theme support */
-require_once( 'library/theme-support.php' );
-
-/** Add Nav Options to Customer */
-require_once( 'library/custom-nav.php' );
-
-/** Change WP's sticky post class */
-require_once( 'library/sticky-posts.php' );
-
-/** Configure responsive image sizes */
-require_once( 'library/responsive-images.php' );
-
-/** If your site requires protocol relative url's for theme assets, uncomment the line below */
-// require_once( 'library/protocol-relative-theme-assets.php' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
